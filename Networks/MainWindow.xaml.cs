022b0fa5f926036eaee0f7fc0e6c2f754a90f449
Misha <misha.vernik1@gmail.com>
2020-10-24 15:11:43 +0300

    Networks graph visualization

---
+++ Networks/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using QuickGraph;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Networks
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private IBidirectionalGraph<object, IEdge<object>> _graphToVisualize;
+
+        public IBidirectionalGraph<object, IEdge<object>> GraphToVisualize
+        {
+            get { return _graphToVisualize; }
+        }
+
+        public MainWindow()
+        {
+            CreateGraphToVisualize();
+            InitializeComponent();
+        }
+
+        private void CreateGraphToVisualize()
+        {
+            var g = new BidirectionalGraph<object, IEdge<object>>();
+
+            //add the vertices to the graph
+            string[] vertices = new string[15];
+            for (int i = 0; i < 15; i++)
+            {
+                vertices[i] = i.ToString();
+                g.AddVertex(vertices[i]);
+            }
+
+            //add some edges to the graph
+            g.AddEdge(new Edge<object>(vertices[0], vertices[1]));
+            g.AddEdge(new Edge<object>(vertices[1], vertices[2]));
+            g.AddEdge(new Edge<object>(vertices[2], vertices[3]));
+            g.AddEdge(new Edge<object>(vertices[3], vertices[1]));
+            g.AddEdge(new Edge<object>(vertices[1], vertices[4]));
+            g.AddEdge(new Edge<object>(vertices[1], vertices[7]));
+            g.AddEdge(new Edge<object>(vertices[2], vertices[11]));
+            g.AddEdge(new Edge<object>(vertices[3], vertices[7]));
+            g.AddEdge(new Edge<object>(vertices[6], vertices[7]));
+            g.AddEdge(new Edge<object>(vertices[8], vertices[9]));
+            g.AddEdge(new Edge<object>(vertices[7], vertices[14]));
+            g.AddEdge(new Edge<object>(vertices[11], vertices[13]));
+
+            _graphToVisualize = g;
+        }
+      
+    }
+}
